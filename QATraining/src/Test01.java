5f5bfb93a39c801245e8448994ae7850b48ec11f
puneeth chidura <puneethchidura123@gmail.com>
2022-12-21 20:24:17 +0530

    made changes to Test01.java

--- QATraining/src/Test01.java
+++ QATraining/src/Test01.java
@@ -3,7 +3,7 @@ public class Test01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hello world");
+System.out.println("hello world, this is puneeth chidura");
 	}
 
 }
